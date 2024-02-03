da1cfc23e5ec76564364aa1b4a394656de846382
Jesse Shawl <jesse@jesse.sh>
2024-02-03 05:59:49 -0600

    fix tests

--- spec/minisign_spec.rb
+++ spec/minisign_spec.rb
@@ -2,14 +2,14 @@
 
 describe Minisign::PublicKey do
   before do
-    @pk = Minisign::PublicKey.new(File.read('test/local.pub').split("\n").pop)
+    @pk = Minisign::PublicKey.new(File.read('test/minisign.pub').split("\n").pop)
     @message = File.read('test/example.txt')
   end
   it 'verifies signatures' do
     @signature = Minisign::Signature.new(File.read('test/example.txt.minisig'))
     expect(@pk.verify(@signature, @message)).to match('Trusted comment')
   end
-  it 'raises ed25519 errors' do
+  it 'raises ed25519 errors for valid signatures but mismatching content' do
     @signature = Minisign::Signature.new(File.read('test/example.txt.minisig.unverifiable'))
     expect { @pk.verify(@signature, @message) }.to raise_error(Ed25519::VerifyError)
   end
@@ -18,20 +18,20 @@ describe Minisign::PublicKey do
     expect { @pk.verify(@signature, @message) }.to raise_error('Comment signature verification failed')
   end
   it 'has a key_id' do
-    expect(@pk.key_id).to eq('E86FECED695E8E0')
+    expect(@pk.key_id).to eq('4CB7A94FABA329A6')
   end
   it 'raises errors on key id mismatch' do
     @pk = Minisign::PublicKey.new('RWQIoBiLxWlf8dGe/DM+igVgetlwOuhWW3abyI1z8eS1RHJVc4o+1sCI')
     @signature = Minisign::Signature.new(File.read('test/example.txt.minisig'))
     expect do
       @pk.verify(@signature, @message)
-    end.to raise_error("Signature key id is E86FECED695E8E0\nbut the key id in the public key is F15F69C58B18A08")
+    end.to raise_error("Signature key id is 4CB7A94FABA329A6\nbut the key id in the public key is F15F69C58B18A08")
   end
 end
 
 describe Minisign::Signature do
   it 'has a key id' do
     @signature = Minisign::Signature.new(File.read('test/example.txt.minisig'))
-    expect(@signature.key_id).to eq('E86FECED695E8E0')
+    expect(@signature.key_id).to eq('4CB7A94FABA329A6')
   end
 end
