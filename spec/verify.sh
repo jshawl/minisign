3544465ab86a634ee6d030006f1c60335684a675
Jesse Shawl <jesse@jesse.sh>
2024-02-03 16:15:53 -0600

    download minisign and verify

--- spec/verify.sh
+++ spec/verify.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "which curl: $(which curl)"
-echo "which unzip: $(which unzip)"
-echo "which wget: $(which wget)"
-echo "OSTYPE: $OSTYPE"
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "mac"
+    url="https://github.com/jedisct1/minisign/releases/download/0.11/minisign-0.11-macos.zip"
+    curl -sL $url -o test/generated/minisign.zip
+    unzip -o test/generated/minisign.zip -d test/generated
+    test/generated/minisign -Vm test/generated/*.txt -p test/minisign.pub
 else
-    echo "linux"
+    url="https://github.com/jedisct1/minisign/releases/download/0.11/minisign-0.11-linux.tar.gz"
+    curl -sL $url -o test/generated/minisign.tar.gz
+    tar -xvzf test/generated/minisign.tar.gz -C test/generated
+    test/generated/minisign-linux/x86_64/minisign -Vm test/generated/*.txt -p test/minisign.pub
 fi
