54798af2c4e1a43653c1dd7410ec4caffef5c8ac
Jesse Shawl <jesse@jesse.sh>
2024-02-07 06:55:05 -0600

    signature.to_s

--- spec/minisign/signature_spec.rb
+++ spec/minisign/signature_spec.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 describe Minisign::Signature do
-  it 'has a key id' do
+  before do
     @signature = Minisign::Signature.new(File.read('test/example.txt.minisig'))
+  end
+
+  it 'has a key id' do
     expect(@signature.key_id).to eq('4CB7A94FABA329A6')
+  end
+
+  it 'can be written to a file' do
+    expect(@signature.to_s).to eq(File.read('test/example.txt.minisig'))
   end
 end
