d83d2eb121d8d62ff24ae25f4115dc470ead2c84
Jesse Shawl <jesse@jesse.sh>
2024-02-19 09:02:03 -0600

    fix typo

--- spec/minisign/nacl_spec.rb
+++ spec/minisign/nacl_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 describe Minisign::NaCl do
-  it 'raises LibSodiumDependencyErro if libsodium not installed' do
+  it 'raises LibSodiumDependencyError if libsodium not installed' do
     hash = RbNaCl.send(:remove_const, :Hash)
     expect do
       Minisign::NaCl::Hash::Blake2b.digest('message', { digest_size: 32 })
